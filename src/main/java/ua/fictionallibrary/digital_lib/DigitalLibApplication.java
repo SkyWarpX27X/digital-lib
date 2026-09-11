f3762fbb551f969462e4db27628d7e4ba2ad9765
SkyWarpX27X <mykhajloost@gmail.com>
2026-09-11 19:55:40 +0300

    Initial commit

---
+++ src/main/java/ua/fictionallibrary/digital_lib/DigitalLibApplication.java
@@ -0,0 +1,13 @@
+package ua.fictionallibrary.digital_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DigitalLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/fictionallibrary/digital_lib/DigitalLibApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.fictionallibrary.digital_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
