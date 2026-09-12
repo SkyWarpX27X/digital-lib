b8618c2f2b523f4d0e129b78ccb9bedeeacdf060
SkyWarpX27X <mykhajloost@gmail.com>
2026-09-12 17:55:29 +0300

    Set endpoints for controllers

---
+++ src/main/java/ua/fictionallibrary/digital_lib/controller/DigitizedBookController.java
@@ -0,0 +1,9 @@
+package ua.fictionallibrary.digital_lib.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/digitized-books")
+public class DigitizedBookController {
+}
