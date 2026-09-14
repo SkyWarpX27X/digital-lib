bc679f50571389c37733827ae79e6d3ede8e5828
SkyWarpX27X <mykhajloost@gmail.com>
2026-09-14 17:52:12 +0300

    Validation and Not found exceptions handling

--- src/main/java/ua/fictionallibrary/digital_lib/common/GlobalExceptionHandler.java
+++ src/main/java/ua/fictionallibrary/digital_lib/common/GlobalExceptionHandler.java
@@ -1,8 +1,33 @@
 package ua.fictionallibrary.digital_lib.common;
 
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(DataNotFoundException.class)
+    public ProblemDetail handleDataNotFound(DataNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Data not found");
+        problemDetail.setType(URI.create("urn:problem-type:user-not-found"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidationFail(ValidationException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Failed to validate data");
+        problemDetail.setType(URI.create("urn:problem-type:validation-error"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
 }
