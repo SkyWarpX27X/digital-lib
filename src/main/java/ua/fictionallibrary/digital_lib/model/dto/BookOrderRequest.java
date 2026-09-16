5f9f7ab9d8c76f8df1d2b9aa9dbd1ed822ea3d67
NazariyRomanyuk <naz.romanyuk@gmail.com>
2026-09-16 13:54:22 +0300

    Add comment for email in BookOrderRequest

--- src/main/java/ua/fictionallibrary/digital_lib/model/dto/BookOrderRequest.java
+++ src/main/java/ua/fictionallibrary/digital_lib/model/dto/BookOrderRequest.java
@@ -1,6 +1,7 @@
 package ua.fictionallibrary.digital_lib.model.dto;
 
 import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import ua.fictionallibrary.digital_lib.common.validation.OnCreate;
 
@@ -9,6 +10,8 @@ import java.util.UUID;
 public record BookOrderRequest(
         @NotNull(message = "User who placed order must be specified")
         UUID creatorId,
+        // Email not yet validated due to the existence of a default option for no input (taken from creatorId).
+        // Will be handled on service level later.
         String emailForBook,
         @NotNull(message = "Book must be specified")
         UUID book,
