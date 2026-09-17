43aaa668d34dbceedf1852bcf3e7e98b343eda98
SkyWarpX27X <mykhajloost@gmail.com>
2026-09-17 18:36:18 +0300

    Connect book order service to controller

--- src/main/java/ua/fictionallibrary/digital_lib/controller/BookOrderController.java
+++ src/main/java/ua/fictionallibrary/digital_lib/controller/BookOrderController.java
@@ -4,12 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import ua.fictionallibrary.digital_lib.exception.DataNotFoundException;
 import ua.fictionallibrary.digital_lib.common.validation.OnCreate;
 import ua.fictionallibrary.digital_lib.common.validation.OnUpdate;
 import ua.fictionallibrary.digital_lib.model.dto.BookOrderRequest;
 import ua.fictionallibrary.digital_lib.model.dto.BookOrderResponse;
 import ua.fictionallibrary.digital_lib.model.entity.BookOrderEntity;
+import ua.fictionallibrary.digital_lib.service.BookOrderService;
 
 import java.net.URI;
 import java.util.*;
@@ -17,44 +17,33 @@ import java.util.*;
 @RestController
 @RequestMapping("/api/v1/book-orders")
 public class BookOrderController {
+    private final BookOrderService service;
 
-    private final Map<UUID, BookOrderEntity> bookOrders;
-
-    public BookOrderController(){
-        this.bookOrders = new HashMap<>();
+    public BookOrderController(BookOrderService service){
+        this.service = service;
     }
 
     @PostMapping
     public ResponseEntity<BookOrderResponse> createBookOrder(@Validated(OnCreate.class) @RequestBody BookOrderRequest request){
         UUID id = UUID.randomUUID();
-        BookOrderEntity entity = new BookOrderEntity(id, request);
-        bookOrders.put(id, entity);
+        BookOrderResponse response = service.addBookOrder(new BookOrderEntity(id, request));
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
-        return ResponseEntity.created(location).body(new BookOrderResponse(entity));
+        return ResponseEntity.created(location).body(response);
     }
 
     @GetMapping
     public ResponseEntity<List<BookOrderResponse>> getBookOrders(@RequestParam(defaultValue = "true") boolean onlyOpen){
-        List<BookOrderResponse> bookOrderResponses = bookOrders.values().stream()
-                .filter(order -> !onlyOpen || order.isOpen()).map(BookOrderResponse::new).toList();
-        return ResponseEntity.ok(bookOrderResponses);
+        return ResponseEntity.ok(service.getBookOrders(onlyOpen));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BookOrderResponse> getBookOrder(@PathVariable UUID id){
-        BookOrderEntity entity = bookOrders.get(id);
-        if(entity == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(new BookOrderResponse(entity));
+        return ResponseEntity.ok(service.getBookOrder(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<BookOrderResponse> updateBookOrder(@PathVariable UUID id, @Validated(OnUpdate.class) @RequestBody BookOrderRequest request){
-        if(!bookOrders.containsKey(id))
-            throw new DataNotFoundException("Failed to update, not found book order with id " + id);
-        BookOrderEntity newValue = new BookOrderEntity(id, request);
-        bookOrders.put(id, newValue);
-        return ResponseEntity.ok(new BookOrderResponse(newValue));
+        BookOrderResponse response = service.updateBookOrder(new BookOrderEntity(id, request));
+        return ResponseEntity.ok(response);
     }
 }
