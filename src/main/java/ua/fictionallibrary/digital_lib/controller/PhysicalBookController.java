e5b8508d4a621a6b4d422c71fac0f830a935056b
SkyWarpX27X <mykhajloost@gmail.com>
2026-09-17 17:35:39 +0300

    Connect physical book service to controller

--- src/main/java/ua/fictionallibrary/digital_lib/controller/PhysicalBookController.java
+++ src/main/java/ua/fictionallibrary/digital_lib/controller/PhysicalBookController.java
@@ -4,12 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import ua.fictionallibrary.digital_lib.exception.DataNotFoundException;
 import ua.fictionallibrary.digital_lib.common.validation.OnCreate;
 import ua.fictionallibrary.digital_lib.common.validation.OnUpdate;
 import ua.fictionallibrary.digital_lib.model.dto.PhysicalBookRequest;
 import ua.fictionallibrary.digital_lib.model.dto.PhysicalBookResponse;
 import ua.fictionallibrary.digital_lib.model.entity.PhysicalBookEntity;
+import ua.fictionallibrary.digital_lib.service.PhysicalBookService;
 
 import java.net.URI;
 import java.util.*;
@@ -17,50 +17,40 @@ import java.util.*;
 @RestController
 @RequestMapping("/api/v1/physical-books")
 public class PhysicalBookController {
-    private Map<UUID, PhysicalBookEntity> physicalBooks;
+    private final PhysicalBookService service;
 
-    public PhysicalBookController() {
-        physicalBooks = new HashMap<>();
+    public PhysicalBookController(PhysicalBookService service) {
+        this.service = service;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PhysicalBookResponse> getBookById(@PathVariable UUID id) {
-        PhysicalBookEntity book = physicalBooks.get(id);
-        if (book == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(new PhysicalBookResponse(book));
+        PhysicalBookResponse book = service.getPhysicalBook(id);
+        return ResponseEntity.ok(book);
     }
 
     @GetMapping
     public ResponseEntity<List<PhysicalBookResponse>> getBooks() {
-        List<PhysicalBookResponse> books = physicalBooks.values().stream().map(PhysicalBookResponse::new).toList();
-        return ResponseEntity.ok(books);
+        return ResponseEntity.ok(service.getAllPhysicalBooks());
     }
 
     @PostMapping
     public ResponseEntity<PhysicalBookResponse> createBook(@Validated(OnCreate.class) @RequestBody PhysicalBookRequest request) {
         UUID id = UUID.randomUUID();
-        PhysicalBookEntity entity = new PhysicalBookEntity(id, request);
-        physicalBooks.put(id, entity);
+        PhysicalBookResponse response = service.addPhysicalBook(new PhysicalBookEntity(id, request));
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
-        return ResponseEntity.created(location).body(new PhysicalBookResponse(entity, id));
+        return ResponseEntity.created(location).body(response);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PhysicalBookResponse> updateBook(@PathVariable UUID id, @Validated(OnUpdate.class) PhysicalBookRequest request) {
-        if (!physicalBooks.containsKey(id))
-            throw new DataNotFoundException("Failed to update, not found physical book with id " + id);
-        PhysicalBookEntity newValue = new PhysicalBookEntity(id, request);
-        physicalBooks.put(id, newValue);
-        return ResponseEntity.ok(new PhysicalBookResponse(newValue));
+        PhysicalBookResponse response = service.updatePhysicalBook(id, new PhysicalBookEntity(id, request));
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<PhysicalBookResponse> deleteBook(@PathVariable UUID id) {
-        if (!physicalBooks.containsKey(id))
-            throw new DataNotFoundException("Failed to delete, not found physical book with id " + id);
-        physicalBooks.remove(id);
+        service.deletePhysicalBook(id);
         return ResponseEntity.noContent().build();
     }
 }
