c75e9b41e8d337efd5250636dacfb35a6e9c2fdc
wwhasmile <uwuhasmile@proton.me>
2026-09-23 15:34:47 +0300

    User controller

--- src/main/java/ua/fictionallibrary/digital_lib/user/UserController.java
+++ src/main/java/ua/fictionallibrary/digital_lib/user/UserController.java
@@ -20,42 +20,35 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    private final Map<UUID, UserEntity> users;
 
-    public UserController() {
-        users = new HashMap<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@Validated(OnCreate.class) @RequestBody UserRequest userRequest) {
-        UUID id = UUID.randomUUID();
-        UserEntity entity = new UserEntity(id, userRequest);
-        users.put(id, entity);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
-        return ResponseEntity.created(location).body(new UserResponse(entity));
+        final var response = userService.addUser(userRequest);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.id()).toUri();
+        return ResponseEntity.created(location).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
-        UserEntity entity = users.get(id);
-        if (entity == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(new UserResponse(entity));
+        final var response = userService.getUser(id);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getUsers() {
-        List<UserResponse> userList = users.values().stream().map(UserResponse::new).toList();
-        return ResponseEntity.ok(userList);
+        final var response = userService.getAllUsers();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable UUID id, @Validated(OnUpdate.class) @RequestBody UserRequest userRequest) {
-        if (!users.containsKey(id))
-            throw new DataNotFoundException("Failed to update, not user found with id: " + id);
-        UserEntity newValue = new UserEntity(id, userRequest);
-        users.put(id, newValue);
-        return ResponseEntity.ok(new UserResponse(newValue));
+        final var response = userService.updateUser(id, userRequest);
+        return ResponseEntity.ok(response);
     }
 }
