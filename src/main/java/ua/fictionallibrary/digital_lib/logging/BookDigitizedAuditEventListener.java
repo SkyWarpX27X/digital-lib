bb597c52b30124e600045bd200e1dd0bb684e710
NazariyRomanyuk <naz.romanyuk@gmail.com>
2026-09-23 19:35:31 +0300

    Add BookDigitizedAuditEventListener

---
+++ src/main/java/ua/fictionallibrary/digital_lib/logging/BookDigitizedAuditEventListener.java
@@ -0,0 +1,14 @@
+﻿package ua.fictionallibrary.digital_lib.logging;
+
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Component;
+import ua.fictionallibrary.digital_lib.digitizedbook.BookDigitizedEvent;
+
+@Component
+public class BookDigitizedAuditEventListener {
+    @ApplicationModuleListener
+    public void onBookDigitized(BookDigitizedEvent event) {
+        System.out.println("Оцифровано фізичну книгу з id: " + event.physicalId() + ". ID оцифрованої книги: "
+                + event.digitizedId());
+    }
+}
