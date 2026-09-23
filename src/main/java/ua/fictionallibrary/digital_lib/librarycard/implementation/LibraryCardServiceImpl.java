f958f7bd7d5fcc5da0baad2ceaf5a3b1be87b462
wwhasmile <uwuhasmile@proton.me>
2026-09-23 15:35:21 +0300

    Style renaming in library card service

--- src/main/java/ua/fictionallibrary/digital_lib/librarycard/implementation/LibraryCardServiceImpl.java
+++ src/main/java/ua/fictionallibrary/digital_lib/librarycard/implementation/LibraryCardServiceImpl.java
@@ -10,23 +10,22 @@ import ua.fictionallibrary.digital_lib.librarycard.model.dto.LibraryCardRequest;
 import ua.fictionallibrary.digital_lib.librarycard.model.dto.LibraryCardResponse;
 import ua.fictionallibrary.digital_lib.user.UserService;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
 public class LibraryCardServiceImpl implements LibraryCardService {
 
-    private final LibraryCardRepository repository;
+    private final LibraryCardRepository libraryCardRepository;
     private final UserService userService;
 
     public LibraryCardServiceImpl(LibraryCardRepository repository, UserService userService) {
-        this.repository = repository;
+        this.libraryCardRepository = repository;
         this.userService = userService;
     }
 
     @Override
     public LibraryCardResponse getLibraryCard(UUID userId) {
-        return repository.getLibraryCard(userId)
+        return libraryCardRepository.getLibraryCard(userId)
                 .map(this::toResponse)
                 .orElseThrow(() -> new DataNotFoundException("Library card for user id " + userId + " doesn't exist"));
     }
@@ -35,36 +34,36 @@ public class LibraryCardServiceImpl implements LibraryCardService {
     public LibraryCardResponse addLibraryCard(UUID userId, LibraryCardRequest card) {
         if (!userService.exists(userId))
             throw new DataNotFoundException("Invalid user " + userId + " doesn't exist");
-        if (repository.exists(userId))
+        if (libraryCardRepository.exists(userId))
             throw new DuplicateException("Digital card for user " + userId + " already exists");
 
-        return toResponse(repository.saveLibraryCard(toEntity(userId, card)));
+        return toResponse(libraryCardRepository.saveLibraryCard(toEntity(userId, card)));
     }
 
     @Override
     public LibraryCardResponse updateLibraryCard(UUID userId, LibraryCardRequest card) {
-        if (!repository.exists(userId))
+        if (!libraryCardRepository.exists(userId))
             throw new DuplicateException("Digital card for user " + userId + " doesn't exist");
-        return toResponse(repository.saveLibraryCard(toEntity(userId, card)));
+        return toResponse(libraryCardRepository.saveLibraryCard(toEntity(userId, card)));
     }
 
     @Override
     public String getEmail(UUID userId) {
-        return repository.getLibraryCard(userId)
+        return libraryCardRepository.getLibraryCard(userId)
                 .map(LibraryCardEntity::email)
                 .orElseThrow(() -> new DataNotFoundException("Library card for user id " + userId + " doesn't exist"));
     }
 
     @Override
     public void deleteLibraryCard(UUID userId) {
-        if (!repository.exists(userId))
+        if (!libraryCardRepository.exists(userId))
             throw new DataNotFoundException("Can't delete non-existent library card for user " + userId);
-        repository.deleteLibraryCard(userId);
+        libraryCardRepository.deleteLibraryCard(userId);
     }
 
     @Override
     public boolean exists(UUID userId) {
-        return repository.exists(userId);
+        return libraryCardRepository.exists(userId);
     }
 
     private LibraryCardEntity toEntity(UUID userId, LibraryCardRequest request) {
