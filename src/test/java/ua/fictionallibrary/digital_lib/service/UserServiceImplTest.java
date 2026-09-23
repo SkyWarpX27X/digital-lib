3bf039ca152e3ea43eab6f6895e98a3f1a00a897
wwhasmile <uwuhasmile@proton.me>
2026-09-23 19:41:47 +0300

    Acdded user service tests

---
+++ src/test/java/ua/fictionallibrary/digital_lib/service/UserServiceImplTest.java
@@ -0,0 +1,83 @@
+package ua.fictionallibrary.digital_lib.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ua.fictionallibrary.digital_lib.exception.DataNotFoundException;
+import ua.fictionallibrary.digital_lib.user.UserRepository;
+import ua.fictionallibrary.digital_lib.user.UserService;
+import ua.fictionallibrary.digital_lib.user.implementation.UserServiceImpl;
+import ua.fictionallibrary.digital_lib.user.model.UserEntity;
+import ua.fictionallibrary.digital_lib.user.model.UserRole;
+import ua.fictionallibrary.digital_lib.user.model.dto.UserRequest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    private UserService service;
+
+    @BeforeEach
+    public void setup() {
+        service = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    void successfullyAddedUser() {
+        final var userRequest = new UserRequest(
+                "wwnothasmile",
+                "09254a@!_m~~",
+                "Illia",
+                "Illienko",
+                "Illiovych",
+                UserRole.READER,
+                true);
+        final var user = new UserEntity(UUID.randomUUID(), userRequest);
+        when(userRepository.saveUser(user)).thenReturn(user);
+        when(userRepository.exists(user.id())).thenReturn(true);
+
+        final var response = service.addUser(user);
+        assertNotNull(response);
+        assertEquals(response.name(), user.name());
+        assertEquals(response.surname(), user.surname());
+        assertEquals(response.patronymic(), user.patronymic());
+        assertEquals(response.role(), user.role());
+        assert(service.exists(response.id()));
+
+        verify(userRepository).saveUser(user);
+    }
+
+    @Test
+    void updatingNonExistentUserThrows() {
+        final var userRequest = new UserRequest(
+                "wwnothasmile",
+                "09254a@!_m~~",
+                "Illia",
+                "Illienko",
+                "Illiovych",
+                UserRole.READER,
+                true);
+        final var user = new UserEntity(UUID.randomUUID(), userRequest);
+        when(userRepository.exists(user.id())).thenReturn(false);
+
+        assertThrows(DataNotFoundException.class, () -> service.updateUser(user.id(), user));
+    }
+
+    @Test
+    void deletingNonExistentThrows() {
+        final var uuid = UUID.randomUUID();
+        when(userRepository.exists(uuid)).thenReturn(false);
+
+        assertThrows(DataNotFoundException.class, () -> service.deleteUser(uuid));
+    }
+}
